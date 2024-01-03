a17cb70caf29d801b4ed6637b0ee2564b665c84a
Marizza Karlin <pinayapapaya@MacBook-Air-som-tillhor-Marizza.local>
2024-01-03 14:29:39 +0100

    paths to the icons have been added

---
+++ src/ts/icons.ts
@@ -0,0 +1,6 @@
+export const icon1Path: string = "path/to/icons/shoppingbag.svg";
+export const icon2Path: string = "path/to/icons/star.svg";
+export const icon3Path: string = "path/to/icons/magnifier.svg";
+export const icon4Path: string = "path/to/icons/minus.svg";
+export const icon5Path: string = "path/to/icons/plus.svg";
+export const icon6Path: string = "path/to/icons/left_arrow.svg";
