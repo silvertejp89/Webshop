7b6bdaec78dc16c6c808649c279f870d9683e69d
Marizza Karlin <pinayapapaya@MacBook-Air-som-tillhor-Marizza.local>
2024-01-03 15:04:56 +0100

    added and updated icons

--- src/ts/icons.ts
+++ src/ts/icons.ts
@@ -3,4 +3,6 @@ export const icon2Path: string = "path/to/icons/star.svg";
 export const icon3Path: string = "path/to/icons/magnifier.svg";
 export const icon4Path: string = "path/to/icons/minus.svg";
 export const icon5Path: string = "path/to/icons/plus.svg";
-export const icon6Path: string = "path/to/icons/left_arrow.svg";
+export const icon6Path: string = "path/to/icons/arrow_left.svg";
+export const icon7Path: string = "path/to/icons/arrow_down.svg";
+export const icon8Path: string = "path/to/icons/cross.svg";
