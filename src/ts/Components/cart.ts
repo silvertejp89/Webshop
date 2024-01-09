77530d1d7262241345dbe84559989f82f6cf0eaf
Kriss Stockhaus <kriss.stockhaus@gmail.com>
2024-01-09 14:07:44 +0100

    fix problems

--- src/ts/Components/cart.ts
+++ src/ts/Components/cart.ts
@@ -1,51 +1,73 @@
-//anpassa koden efter de riktiga objekten. 
 //Ska carten sparas i localstorage? antagligen. 
 
+import "../../scss/main.scss"
 import { CartProduct } from "../models/CartProduct";
 
 
-import "../../scss/main.scss"
-
-export let cart: CartProduct[] = [];
+export let cart: CartProduct[] = []
 
 //mock-data-------------------------------------------------------------------------
-const mock: {id: number; name: string; thumbnail_image: string; price: number}[] = [
-    {
-        id: 8,
-        name: "Noa",
-        thumbnail_image: "src/images/photos/Noa_product_image.png",
-        price: 10,
-    },
-    {
-        id: 9,
-        name: "Iris",
-        thumbnail_image: "src/images/photos/Iris_product_image.png",
-        price: 20,
-    },
-    {
-      id: 10,
-      name: "Greta",
-      thumbnail_image: "src/images/photos/Iris_product_image.png",
-      price: 30,
-  },
-  {
-    id: 11,
-    name: "Chlaes",
-    thumbnail_image: "src/images/photos/Iris_product_image.png",
-    price: 40,
+const mock: {id: number; name: string; thumbnail_image: string; image_back: string; image_side: string; 
+  color: string; priceSEK: number, description: string; measurement: string, size: string}[] = [
+  {   
+    "id": 1,
+    "name": "Olof",
+    "thumbnail_image": "src/images/photos/Olof_product_image.png",
+    "image_back": "src/images/photos/Olof_back.png",
+    "image_side": "src/images/photos/Olof_side.png",
+    "color": "Black",
+    "priceSEK": 895.00,
+    "description": "Funktionell och vattenavvisande cross-body väska tillverkad i 100% återvunnen polyester. Väskan är designad med två innerfickor, en avtagbar och justerbar axelrem och ett handtag. En perfekt extraväska för de dagar du bara behöver packa det viktigaste.",
+    "measurement": "24 x 16 x 9 cm",
+    "size": "One size"
+},
+{
+    "id": 2,
+    "name": "Dante Vegan",
+    "thumbnail_image": "src/images/photos/Dante_product_image.png",
+    "image_back": "src/images/photos/Dante_back_picture.png",
+    "image_side": "src/images/photos/Dante_side_product.png",
+    "color": "Black",
+    "priceSEK": 1395.00,
+    "description": "Dante Vegan är en rymlig och vattenavvisande rolltop-ryggsäck skapad för att kunna användas varje dag, oavsett om du pendlar till jobbet eller är ute i naturen. Dante Vegan en variant av vår bästsäljande modell Dante, men gjord utan läderdetaljer och i 100% återvunna och veganska material. Väskan stängs med ett spänne i metall och rymmer mellan 16–23 L. Ryggsäcken har två innerfack, varav ett passar de flesta datorer upp till 16 tum. Vadderade axelremmar för skön bärkomfort och ytterfack med dragkedja, samt handtag på sidan. Det här är en hållbar ryggsäck med hög kvalitet och tidlös design, som du kan använda och älska länge. Dante Vegan är en del av vår Ground-kollektion. Här hittar du fler väskor i samma serie.",
+    "measurement": "26 x 43/56 x 16 cm",
+    "size": "One size"
+},
+{
+    "id": 3,
+    "name": "Tony Vegan",
+    "thumbnail_image": "src/images/photos/Tony_product_image.png",
+    "image_back": "src/images/photos/Tony_back.png",
+    "image_side": "src/images/photos/Tony_side.png",
+    "color": "Green",
+    "priceSEK": 1295.00,
+    "description": "Ryggsäck i 100 % återvunnen polyester med vattentät TPU-beläggning. Väskan har ett 15' laptopfack, en yttrerficka med blixtlås och kan bära upp till 13 L. Funktionaliteten och den minimalistiska designen gör denna ryggsäck till en idealisk följeslagare, vart du än går.",
+    "measurement": "27 x 42 x 12 cm",
+    "size": "One size"
+},
+{
+    "id": 4,
+    "name": "Kurt",
+    "thumbnail_image": "src/images/photos/Kurt_product_image.png",
+    "image_back": "src/images/photos/Kurt_back.png",
+    "image_side": "src/images/photos/Kurt_side.png",
+    "color": "Pink",
+    "priceSEK": 1295.00,
+    "description": "Kurt är en minimalistisk, mångsidig och slitstark rolltop-ryggsäck med fina läderdetaljer. Den rymmer upp till 17 L och är gjord av vattenavvisande och 100% återvunnen polyester. Kurt har justerbara axelremmar, ett dolt ytterfack med dragkedja, samt ett laptopfack som passar de flesta datorer upp till 16 tum. Denna ryggsäck är perfekt att använda varje dag, oavsett om du pendlar till jobbet eller är ute i naturen. Kurt är ett av de senaste tillskotten i vår Ground-kollektion. Här hittar du fler väskor i samma serie.",
+    "measurement": "27 x 38 x 15 cm",
+    "size": "One size"
 }
 ];
-
+//----------------------------------------------------
 
 const mockContainer = document.querySelector(".mock-container");
 const cartContainer = document.querySelector(".cart-container");
 
-
-//enkel createHTML kanske sedan kan heta createHTMLProducts?------------------------------
-export function createHTMLMock(): void {
+  
+export function createHTMLProducts(): void {
 
     //Använda "Non-null assertion operator" för att undvika squigglies? 
-    
+    if(!mockContainer) return;
     mockContainer.innerHTML = "";
     for (let i = 0; i < mock.length; i++) {
       const card = document.createElement("article");
@@ -64,18 +86,19 @@ export function createHTMLMock(): void {
       card.appendChild(text);
       card.appendChild(priceText);
       card.appendChild(submitBtn);
-      mockContainer.appendChild(card);
+      mockContainer?.appendChild(card);
   
       text.innerHTML = mock[i].name;
-      priceText.innerHTML = String(mock[i].price) + "kr";
+      priceText.innerHTML = String(mock[i].priceSEK) + " kr";
 
     }
   };
 
-  createHTMLMock();
+  createHTMLProducts();
 
   //CART HTML------------------------------------------------------------------------
 export function createHTMLCart() {
+  if(!cartContainer) return;
     cartContainer.innerHTML = "";
 
 
@@ -107,7 +130,7 @@ export function createHTMLCart() {
       //mängd och totalpris per produkttyp
       const amountText = document.createElement("h2");
       card.appendChild(amountText);
-      amountText.innerHTML = String(cart[i].amount) + " st, " + String((cart[i].price)*(cart[i].amount)) + "kr";
+      amountText.innerHTML = String(cart[i].amount) + " st, " + String((cart[i].priceSEK)*(cart[i].amount)) + "kr";
 
       //ta bort-knapp
       const removeBtn = document.createElement("button");
@@ -117,30 +140,30 @@ export function createHTMLCart() {
       });
       card.appendChild(removeBtn);
       //Lägg card i container
-      cartContainer.appendChild(card);
+      cartContainer?.appendChild(card);
            
     }
-    createHTMLMock();
+    createHTMLProducts();
     //Rubrik 
     calculateAmount();
     
     calculateTotalPrice();
 
     // dessa har hittats med queryselector längst ner. 
+    if(!amountCounter) return;
     amountCounter.textContent = `${totalAmount}` + " produkter, ";
+    if(!priceCounter) return;
     priceCounter.textContent = `${totalPrice}` + " kr";
-
-    
   };
 
   //LÄGG TILL-knapp-funktion------------------------------
   export function createCartProduct(i: number) {
-
     const id = mock[i].id;
     const name = mock[i].name;
     const thumbnail_image = mock[i].thumbnail_image;
-    const amount = mock[i].amount;
-    const price = mock[i].price;
+    // const amount = mock[i].amount;
+    const price = mock[i].priceSEK;
+    const color = mock[i].color;
     //------------------------------------------------------------------------------
 
     //TODO: testa todo-funktionen
@@ -148,25 +171,20 @@ export function createHTMLCart() {
     //Om id redan existerar i cart[] ska det inte skapas ett nytt objekt.
     //dvs. loopa genom cart-listan och jämför med mock[i].id,
 
-    let foundObject = null; 
-    cart.forEach((product) => {
+    let foundProductInCart = false;
+    cart.forEach((product, i) => {
       if (product.id === id) {
-        foundObject = product;
+        cart[i].amount++;
+        foundProductInCart = true;
       }
     });
 
-    if (foundObject) {
-      foundObject.amount++;
-      console.log("Objekt finns redan:", foundObject);
-      console.log(foundObject.amount);
-      createHTMLCart();
-    } else {
-      const newCartProduct = new CartProduct(id, name, thumbnail_image, amount, price)
-
+    if (!foundProductInCart) {
+      const newCartProduct = new CartProduct(id, name, thumbnail_image, color, price)
       cart.push(newCartProduct);
-      createHTMLCart();
       console.log("Din varukorg: ", cart.length, cart);
-    }
+    } 
+    createHTMLCart();
   };
 
 
@@ -212,10 +230,10 @@ export function calculateTotalPrice() {
 
   for (let i = 0; i < cart.length; i++) {
     console.log("cart amount: ",cart[i].amount)
-    console.log("cart price: ",cart[i].price)
+    console.log("cart price: ",cart[i].priceSEK)
 
 
-    totalPrice += cart[i].amount * cart[i].price;
+    totalPrice += cart[i].amount * cart[i].priceSEK;
     console.log("total price:" ,totalPrice);
   }
 };
--- src/ts/models/CartProduct.ts
+++ src/ts/models/CartProduct.ts
@@ -1,38 +1,22 @@
-// //class för att skapa "ny" produkt att lägga i varukorgen. Amount sätts till 1 som default. 
+//class för att skapa "ny" produkt att lägga i varukorgen. 
+//Amount lägg till och sätts till 1 som default. "Description" och "measurement" finns inte med.
 
-// export class CartProduct {
-//     id: number;
-//     name: string;
-//     thumbnail_image: string;
-//     color: string;
-//     size: string;
-//     priceSEK: number;
-//     amount: number;
-  
-//     constructor(id: number, name: string, thumbnail_image: string, color: string, size: string, priceSEK: number, amount: number = 1) {
-//       this.id = id;
-//       this.name = name;
-//       this.thumbnail_image = thumbnail_image;
-//       this.color = color;
-//       this.size = size;
-//       this.priceSEK = priceSEK;
-//       this.amount = amount;
-//     }
-//   }
-
-//förenklad klass--------------------------------------------------------------
 export class CartProduct {
     id: number;
     name: string;
     thumbnail_image: string;
+    color: string;
+    priceSEK: number;
+    // size: string;
     amount: number;
-    price: number;
   
-    constructor(id: number, name: string, thumbnail_image: string, amount: number = 1, price: number) {
+    constructor(id: number, name: string, thumbnail_image: string, color: string, priceSEK: number, amount: number = 1) {
       this.id = id;
       this.name = name;
       this.thumbnail_image = thumbnail_image;
-      this.amount = amount; 
-      this.price = price;
+      this.color = color;
+      this.priceSEK = priceSEK;
+    //   this.size = size; //glöm inte att lägga till o rad 13 också.
+      this.amount = amount;
     }
   }
