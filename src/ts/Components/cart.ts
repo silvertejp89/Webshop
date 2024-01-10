32a96f9956e97ad8c6eca4ed400b0efaaccdca84
Kriss Stockhaus <kriss.stockhaus@gmail.com>
2024-01-10 16:03:23 +0100

    add thumnail-images

--- src/ts/Components/cart.ts
+++ src/ts/Components/cart.ts
@@ -23,9 +23,9 @@ const mock: {id: number; name: string; thumbnail_image: string; image_back: stri
   {   
     "id": 1,
     "name": "Olof",
-    "thumbnail_image": "src/images/photos/Olof_product_image.png",
-    "image_back": "src/images/photos/Olof_back.png",
-    "image_side": "src/images/photos/Olof_side.png",
+    "thumbnail_image": "src/images/photos/png/Olof_product_image.png",
+    "image_back": "src/images/photos/png/Olof_back.png",
+    "image_side": "src/images/photos/png/Olof_side.png",
     "color": "Black",
     "priceSEK": 895.00,
     "description": "Funktionell och vattenavvisande cross-body väska tillverkad i 100% återvunnen polyester. Väskan är designad med två innerfickor, en avtagbar och justerbar axelrem och ett handtag. En perfekt extraväska för de dagar du bara behöver packa det viktigaste.",
@@ -35,9 +35,9 @@ const mock: {id: number; name: string; thumbnail_image: string; image_back: stri
 {
     "id": 2,
     "name": "Dante Vegan",
-    "thumbnail_image": "src/images/photos/Dante_product_image.png",
-    "image_back": "src/images/photos/Dante_back_picture.png",
-    "image_side": "src/images/photos/Dante_side_product.png",
+    "thumbnail_image": "src/images/photos/png/Dante_product_image.png",
+    "image_back": "src/images/photos/png/Dante_back_picture.png",
+    "image_side": "src/images/photos/png/Dante_side_product.png",
     "color": "Black",
     "priceSEK": 1395.00,
     "description": "Dante Vegan är en rymlig och vattenavvisande rolltop-ryggsäck skapad för att kunna användas varje dag, oavsett om du pendlar till jobbet eller är ute i naturen. Dante Vegan en variant av vår bästsäljande modell Dante, men gjord utan läderdetaljer och i 100% återvunna och veganska material. Väskan stängs med ett spänne i metall och rymmer mellan 16–23 L. Ryggsäcken har två innerfack, varav ett passar de flesta datorer upp till 16 tum. Vadderade axelremmar för skön bärkomfort och ytterfack med dragkedja, samt handtag på sidan. Det här är en hållbar ryggsäck med hög kvalitet och tidlös design, som du kan använda och älska länge. Dante Vegan är en del av vår Ground-kollektion. Här hittar du fler väskor i samma serie.",
@@ -47,9 +47,9 @@ const mock: {id: number; name: string; thumbnail_image: string; image_back: stri
 {
     "id": 3,
     "name": "Tony Vegan",
-    "thumbnail_image": "src/images/photos/Tony_product_image.png",
-    "image_back": "src/images/photos/Tony_back.png",
-    "image_side": "src/images/photos/Tony_side.png",
+    "thumbnail_image": "src/images/photos/png/Tony_product_image.png",
+    "image_back": "src/images/photos/png/Tony_back.png",
+    "image_side": "src/images/photos/png/Tony_side.png",
     "color": "Green",
     "priceSEK": 1295.00,
     "description": "Ryggsäck i 100 % återvunnen polyester med vattentät TPU-beläggning. Väskan har ett 15' laptopfack, en yttrerficka med blixtlås och kan bära upp till 13 L. Funktionaliteten och den minimalistiska designen gör denna ryggsäck till en idealisk följeslagare, vart du än går.",
@@ -59,9 +59,9 @@ const mock: {id: number; name: string; thumbnail_image: string; image_back: stri
 {
     "id": 4,
     "name": "Kurt",
-    "thumbnail_image": "src/images/photos/Kurt_product_image.png",
-    "image_back": "src/images/photos/Kurt_back.png",
-    "image_side": "src/images/photos/Kurt_side.png",
+    "thumbnail_image": "src/images/photos/png/Kurt_product_image.png",
+    "image_back": "src/images/photos/png/Kurt_back.png",
+    "image_side": "src/images/photos/png/Kurt_side.png",
     "color": "Pink",
     "priceSEK": 1295.00,
     "description": "Kurt är en minimalistisk, mångsidig och slitstark rolltop-ryggsäck med fina läderdetaljer. Den rymmer upp till 17 L och är gjord av vattenavvisande och 100% återvunnen polyester. Kurt har justerbara axelremmar, ett dolt ytterfack med dragkedja, samt ett laptopfack som passar de flesta datorer upp till 16 tum. Denna ryggsäck är perfekt att använda varje dag, oavsett om du pendlar till jobbet eller är ute i naturen. Kurt är ett av de senaste tillskotten i vår Ground-kollektion. Här hittar du fler väskor i samma serie.",
@@ -98,8 +98,6 @@ export function createHTMLProducts(): void {
       card.appendChild(priceText);
       card.appendChild(submitBtn);
       mockContainer?.appendChild(card);
-
-      text.innerHTML = mock[i].name;
       priceText.innerHTML = String(mock[i].priceSEK) + " kr";
 
     }
@@ -114,14 +112,13 @@ export function createHTMLCart() {
 
     for (let i = 0; i < cart.length; i++) {
 
-      console.log("hejsan svejsan ", cart[i]);
       const card = document.createElement("article");
       card.classList.add("card2");
 
       //Thumnail-bild
       const thumnailImage = document.createElement("img");
       card.appendChild(thumnailImage);
-      thumnailImage.innerHTML = cart[i].thumbnail_image;
+      thumnailImage.src = cart[i].thumbnail_image;
 
       //Produktnamn
       const productName = document.createElement("h2");
