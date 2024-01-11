8d45776675700f743e81a58dcf09d23073632049
leonmcvey <leon.mcvey@hyperisland.se>
2024-01-11 21:55:15 +0100

    added test

---
+++ src/ts/Components/cartTest.ts
@@ -0,0 +1,57 @@
+window.addEventListener("load", () => {
+  location.href = "cart.html?id=" + product.id;
+});
+
+interface IProduct {
+  id: number;
+  name: string;
+  thumbnail_image: string;
+  image_back: string;
+  image_side: string;
+  color: string;
+  priceSEK: number;
+  description: string;
+  measurement: string;
+  size: string;
+}
+
+function getProductIdFromUrl() {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get("id");
+}
+
+const productId = getProductIdFromUrl();
+
+function getProductDetails(productId: string): Promise<IProduct | undefined> {
+  return fetch("src/data/products.json")
+    .then((response) => response.json())
+    .then((data: IProduct[]) => {
+      for (const product of data) {
+        if (product.id.toString() === productId) {
+          return product;
+        }
+      }
+      return undefined;
+    })
+    .catch((error) => {
+      console.error("Error fetching product details:", error);
+      return undefined;
+    });
+}
+
+function createProductDetailsHTML(product: IProduct) {
+  return `<img src=${product.thumbnail_image}>`;
+}
+
+if (productDetailsContainer && productId !== null) {
+  getProductDetails(productId).then((productDetails) => {
+    if (productDetails) {
+      productDetailsContainer.innerHTML =
+        createProductDetailsHTML(productDetails);
+    } else {
+      console.error("Product details not available.");
+    }
+  });
+} else {
+  console.error("Product ID not found in the URL");
+}
